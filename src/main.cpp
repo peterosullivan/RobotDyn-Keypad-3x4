a31adf27adf7a85b722700aad15f87393a2dd505
Peter O'Sullivan <peter@peterosullivan.net>
2020-09-11 15:50:54 +1000

    Clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 // OUT -> A0 (Analog 0)
 #include <Keyboard.h>
 
-#define BUTTON_KEY1 KEY_F13
+#define BUTTON_KEY1 KEY_F1 //KEY_F13
 #define BUTTON_KEY2 KEY_F14
 #define BUTTON_KEY3 KEY_F15
 #define BUTTON_KEY4 KEY_F16
@@ -17,16 +17,16 @@
 #define BUTTON_KEY6 KEY_F18
 #define BUTTON_KEY7 KEY_F19
 #define BUTTON_KEY8 KEY_F20
-#define BUTTON_KEY9 KEY_F1
-#define BUTTON_KEY10 KEY_F2
-#define BUTTON_KEY11 KEY_F3
-#define BUTTON_KEY12 KEY_F4
+#define BUTTON_KEY9 KEY_F21
+#define BUTTON_KEY10 KEY_F22
+#define BUTTON_KEY11 KEY_F23
+#define BUTTON_KEY12 KEY_F24
 
-#define BUTTON_1 1023
-#define BUTTON_2 931
-#define BUTTON_3 853
+#define BUTTON_1  1023
+#define BUTTON_2  931
+#define BUTTON_3  853
 
-#define BUTTON_4 783
+#define BUTTON_4  783
 #define BUTTON_5  728
 #define BUTTON_6  679
 
@@ -39,37 +39,52 @@
 #define BUTTON_12 482
 
 // button detection
-int get_button()
-{
-  // get A1 analog input value
-  int val = analogRead(KEYPAD_ANALOG_PIN);
-  if (val >= BUTTON_1) return 1;
-  else if (val >= BUTTON_2) return 2;
-  else if (val >= BUTTON_3) return 3;
-  else if (val >= BUTTON_4) return 4;
-  else if (val >= BUTTON_5) return 5;
-  else if (val >= BUTTON_6) return 6;
-  else if (val >= BUTTON_7) return 7;
-  else if (val >= BUTTON_8) return 8;
-  else if (val >= BUTTON_9) return 9;
-  else if (val >= BUTTON_10) return 10;
-  else if (val >= BUTTON_11) return 11;
-  else if (val >= BUTTON_12) return 12;
+int get_button(){
+
+  int value = analogRead(KEYPAD_ANALOG_PIN);
+
+  if (value >= BUTTON_1)       return BUTTON_KEY1;
+  else if (value >= BUTTON_2)  return BUTTON_KEY2;
+  else if (value >= BUTTON_3)  return BUTTON_KEY3;
+  else if (value >= BUTTON_4)  return BUTTON_KEY4;
+  else if (value >= BUTTON_5)  return BUTTON_KEY5;
+  else if (value >= BUTTON_6)  return BUTTON_KEY6;
+  else if (value >= BUTTON_7)  return BUTTON_KEY7;
+  else if (value >= BUTTON_8)  return BUTTON_KEY8;
+  else if (value >= BUTTON_9)  return BUTTON_KEY9;
+  else if (value >= BUTTON_10) return  BUTTON_KEY10;
+  else if (value >= BUTTON_11) return  BUTTON_KEY11;
+  else if (value >= BUTTON_12) return  BUTTON_KEY12;
   else return 0;
 }
 
+void failsafe(){
+  for (;;){
+  } // Just going to hang out here for awhile :D
+}
+
 void setup(){
-  Serial.begin(9600);
+  //Serial.begin(9600);
+
+  // Safety check. Ground pin #1 (RX) to cancel keyboard inputs.
+  pinMode(1, INPUT_PULLUP);
+  if (!digitalRead(1)){
+    failsafe();
+  }
 }
 
 void loop(){
   // is any button pressed?
-  int valueA1 = get_button();
-  if (valueA1 > 0){
-    Serial.print("Click button ");
-    Serial.print(valueA1);
-    Serial.print(" ");
-    Serial.println(analogRead(KEYPAD_ANALOG_PIN));
-    delay(200);
+  int key = get_button();
+  if (key > 0){
+    
+    //Serial.println(analogRead(KEYPAD_ANALOG_PIN));
+    //Serial.print(key);
+
+    Keyboard.write(key);
+    //Keyboard.press(key);
+    //Keyboard.release(key);
+
+    delay(100);
   }
 }
